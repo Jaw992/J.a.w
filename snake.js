10a79cea2d37c7fb6aabefafd0d9ed298606a4e8
Jaw992 <awtong22@gmail.com>
2024-07-24 16:53:49 +0800

    Updated game looks, add sound effects

--- snake.js
+++ snake.js
@@ -5,7 +5,7 @@ let snake = {x: 15, y:15}; // Starting Postion
 let snakeBody = [];
 let controlX = 0;
 let controlY = 0;
-let food = {x: 27, y: 20}; // Starting Position
+let food = {x: 25, y: 20}; // Starting Position
 
 let score = 0; // Use this to display 
 let highScore = localStorage.getItem("high-score") || 0;
@@ -16,6 +16,8 @@ let renderTime;
 const startPage = document.getElementById("start-page");
 const gamePage = document.getElementById("game-page");
 const endPage = document.getElementById("game-over");
+const popUpElement = document.getElementById("popup");
+
 const gameArea = document.querySelector(".game-area");
 const scoreElement = document.querySelector('.score');
 const highScoreElement = document.querySelector('.high-score');
@@ -29,12 +31,38 @@ function initGame() {
     drawFood();
 }
 
+function renderGame() {    
+    if (gameState === true) {
+        endPage.style.display = "block";
+        clearInterval(renderTime);
+    }
+    // Adds x and y movement to snake when arrow keys are pressed 
+    snake.x += controlX;
+    snake.y += controlY;
+
+    // Check if the snake has meet with the food
+    if (snake.x === food.x && snake.y === food.y) {
+        randomFood();
+        snakeBody.push([food.x, food.y]); // Store an array value to add to snake length
+        getScores();
+    }
+
+    // Shift element value forward by one to snake body
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = [snake.x, snake.y]; // Sets first element of snake body to current position
+
+    endConditions();
+    initGame();
+}
+
 /*-------------------------------- Functions --------------------------------*/
 // Initialise welcome page
 function init() {
     gamePage.style.display = "block";
     endPage.style.display = "none";
-    startPage.style.display = "none";
+    startPage.style.display = "block";
 }
 
 function drawSnake () {
@@ -64,11 +92,11 @@ const randomFood = () => {
     food.y = Math.floor(Math.random() * 30) + 1;
 }
 
-function gameOver() {
-    clearInterval(renderTime);
-    alert("You Lose! Play Again?");
-    location.reload(); // Reload the page
-}
+// function gameOver() {
+//     clearInterval(renderTime);
+//     // alert("You Lose!!");
+//     // location.reload(); // Reload the page
+// }
 
 function getScores () {
     score += 1;
@@ -76,47 +104,31 @@ function getScores () {
     localStorage.setItem("high-score", highScore);
     scoreElement.innerText = `Score: ${score}`;
     highScoreElement.innerText = `High Score: ${highScore}`;
+    this.sound = new Audio();
+    this.sound.src = 'mixkit-quick-lock-sound-2854.wav';
+    this.sound.play();
 }
 
 const endConditions = () => {
         // Cehcking if snake goes over board
         if (snake.x <= 0 || snake.x > 30 || snake.y <= 0 || snake.y > 30) {
+            this.sound = new Audio();
+            this.sound.src = 'mixkit-arcade-fast-game-over-233.wav';
+            this.sound.play();
             gameState = true;
         }
     
         // Checking if snake hits itself
         for (let i = 1; i < snakeBody.length; i++) {
             if (snake.x === snakeBody[i][0] && snake.y === snakeBody[i][1]) {
+                this.sound = new Audio();
+                this.sound.src = 'mixkit-arcade-fast-game-over-233.wav';
+                this.sound.play();
                 gameState = true;
             }
         }
 }
 
-
-function renderGame() {    
-    if (gameState === true) {
-        return gameOver();
-    }
-    // Adds x and y movement to snake when arrow keys are pressed 
-    snake.x += controlX;
-    snake.y += controlY;
-
-    // Check if the snake has meet with the food
-    if (snake.x === food.x && snake.y === food.y) {
-        randomFood();
-        snakeBody.push([food.x, food.y]); // Store an array value to add to snake length
-        getScores();
-    }
-
-    // Shift element value forward by one to snake body
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
-    }
-    snakeBody[0] = [snake.x, snake.y]; // Sets first element of snake body to current position
-
-    endConditions();
-    initGame();
-}
 
 /*----------------------------- Event Listeners -----------------------------*/
 // Goes to game page from start page
@@ -125,6 +137,12 @@ startButton.addEventListener("click", () => {
     startPage.style.display = "none";
     endPage.style.display = "none";
     gamePage.style.display = "block";
+});
+
+const endButton = document.getElementById("end");
+endButton.addEventListener("click", () => {
+    clearInterval(renderTime);
+    location.reload();
 });
 
 // eventlistener to move by arrow key pressed to move snake
@@ -144,6 +162,7 @@ document.addEventListener("keydown", (e) => {
     }
     renderGame();
 });
+
 
 init();
 renderTime = setInterval(renderGame, 100);
